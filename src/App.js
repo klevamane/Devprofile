e2a84ec58c19c629eaceaf166976982d2477bb21
Onengiye Richard <klevamane@gmail.com>
2019-11-03 17:45:19 +0100

    add packages and setup redux

--- src/App.js
+++ src/App.js
@@ -4,23 +4,28 @@ import Navbar from './components/layout/Navbar';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './store';
 
 import './App.css';
 
-const App = () => 
-// Fragment wont show up in the dom
-  <Router>
+const App = () => (
+  // Fragment wont show up in the dom
+  <Provider store={store}>
+    <Router>
       <Fragment>
         <Navbar />
         <Route exact path="/" component={Landing} />
         <section className="container">
           <Switch>
-            <Route exact path="/register" component={Register}/>
-            <Route exact path="/login" component={Login}/>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </section>
       </Fragment>
-  </Router>
-    
+    </Router>
+  </Provider>
+);
 
 export default App;
